a80d3dc01ddd2bcbb830a7726377a984f28eb979
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2023-01-05 19:35:44 +0200

    pelaajien ja pakan alustukset

---
+++ korttipeli.py
@@ -0,0 +1,39 @@
+import random
+
+# luodaan peli kahdelle hengelle
+def allPlayers() -> list:
+    players = []
+    player1 = str(input("Name?"))
+    player2 = str(input("Name?"))
+    players.append(player1)
+    players.append(player2)
+
+    return players
+
+print ("Players are")
+allPlayers()
+
+# luodaan korttipakka pelaamista varten
+def defineDeck() -> list:
+    card = 1
+    deck = []
+    for i in range(1, 14):
+        deck.append(('♤', card))
+        deck.append(('❤', card))
+        deck.append(('✠', card))
+        deck.append(('♢', card))
+        card = card + 1
+
+   #sekoitetaan pakka vielä valmiiksi tässä metodissa
+    deck = shuffle(deck)
+    return deck
+
+
+def shuffle(deck):
+     random.shuffle(deck)
+     return deck
+print(defineDeck())
+
+#tehdään metodi, joka ajetaan joka pelikierroksen välissä = jaetaan pelaajille kortti
+def deal (deck, players)   :
+    
