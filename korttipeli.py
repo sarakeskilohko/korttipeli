7e6565f5e42da2c1dd0c96e72970ccf3f7d30c56
Janne <joriss@utu.fi>
2023-01-06 16:20:22 +0200

    number kysely tehty

--- korttipeli.py
+++ korttipeli.py
@@ -31,14 +31,57 @@ def shuffle(deck):
      return deck
 
 #tehdään metodi, joka ajetaan joka pelikierroksen välissä = jaetaan pelaajille kortti
-def deal (players, deck) -> dict:
-    onecard = {}
+#tehdään metodit erikseen kummallekkin pelaajille, jotta kortit tallennetaan eri sanakirjoihin
+def deal1 (players, deck) -> dict:
+    player1 = {}
     for i in players:
-        onecard[i] = deck[0]
-        deck.remove(onecard[i])
-        return onecard
+        player1[i] = deck[0]
+        deck.remove(player1[i])
+        return player1
+
+def deal2 (players, deck) -> dict:
+    player2 = {}
+    length = len(players)
+    for index in range(1 , length):
+        i = players[index]
+        player2[i] = deck[0]
+        deck.remove(player2[i])
+        return player2
 
 
+def number1(deal1):
+    card1 = deal1(players, deck)
+    guess = int(input("Which number?"))
+    if guess == card1:
+        print("Give one drink!")
+        print("Card was: " + str(card1))
+    else:
+        print("Drink one!")
+        print("Card was: " + str(card1))
+
+def number2(deal2):
+    card2 = deal2(players, deck)
+    guess = int(input("Which number?"))
+    if guess == card2:
+        print("Give one drink!")
+        print("Card was: " + str(card2))
+    else:
+        print("Drink one!")
+        print("Card was: " + str(card2))
+
+
+def compare1(deal1):
+    card = deal1(players, deck)
+    guess = int(input("Higher or lower?"))
+    if guess 
+
+#print(defineDeck())
 players = allPlayers()
 print ("Players are:" + str(players))
-print(defineDeck())
+deck = defineDeck()
+#kortit1 = deal1(players, deck)
+#kortit2 = deal2(players, deck)
+#print(kortit1)
+#print(kortit2)
+number1(deal1)
+number2(deal2)
