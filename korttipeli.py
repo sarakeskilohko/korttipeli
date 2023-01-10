2fae9ee398261cbfe3e503c1c6f2a1a15f0175d3
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2023-01-10 16:01:22 +0200

    muokkauksia

--- korttipeli.py
+++ korttipeli.py
@@ -1,14 +1,17 @@
 import random
 
+
 # luodaan peli kahdelle hengelle
-def allPlayers() -> list:
-    players = []
+def allPlayers() -> dict:
     player1 = str(input("Name?"))
     player2 = str(input("Name?"))
-    players.append(player1)
-    players.append(player2)
+    players = {
+        player1: [],
+        player2: []
+    }
 
     return players
+
 
 # luodaan korttipakka pelaamista varten
 def defineDeck() -> list:
@@ -21,37 +24,34 @@ def defineDeck() -> list:
         deck.append(('♢', card))
         card = card + 1
 
-   #sekoitetaan pakka vielä valmiiksi tässä metodissa
+    # sekoitetaan pakka vielä valmiiksi tässä metodissa
     deck = shuffle(deck)
     return deck
 
 
 def shuffle(deck):
-     random.shuffle(deck)
-     return deck
+    random.shuffle(deck)
+    return deck
 
-#tehdään metodi, joka ajetaan joka pelikierroksen välissä = jaetaan pelaajille kortti
-#tehdään metodit erikseen kummallekkin pelaajille, jotta kortit tallennetaan eri sanakirjoihin
-def deal1 (players, deck) -> dict:
-    player1 = {}
-    for i in players:
-        player1[i] = deck[0]
-        deck.remove(player1[i])
-        return player1
 
-def deal2 (players, deck) -> dict:
-    player2 = {}
-    length = len(players)
-    for index in range(1 , length):
-        i = players[index]
-        player2[i] = deck[0]
-        deck.remove(player2[i])
-        return player2
+# tehdään metodi, joka ajetaan joka pelikierroksen välissä = jaetaan pelaajille kortti
+# tehdään metodit erikseen kummallekkin pelaajille, jotta kortit tallennetaan eri sanakirjoihin
+def deal1(players, deck, playerName) -> (list, dict):
+    card = deck[0]
+    newPlayers = players.copy()
+    newPlayers[playerName].append(card)
+    del deck[0]
+    return (deck, newPlayers)
+
+for playerName, hand in players.items()
+    print playername hand
 
 
 def number1(deal1):
     card1 = deal1(players, deck)
+    print(card1)
     list = card1.keys()
+    print(list)
     card = list[0]
     guess = int(input("Which number?"))
     if guess == card:
@@ -60,6 +60,7 @@ def number1(deal1):
     else:
         print("Wrong, drink one!")
         print("Card was: " + str(card1))
+
 
 def number2(deal2):
     card2 = deal2(players, deck)
@@ -90,6 +91,7 @@ def compare1(deal1):
         print("Wrong, drink one!")
         print("Card was: " + str(card[1]))
 
+
 def compare2(deal2):
     card = deal2(players, deck)
     guess = str(input("Higher, lower or same?"))
@@ -107,16 +109,16 @@ def compare2(deal2):
         print("Card was: " + str(card[1]))
 
 
-#print(defineDeck())
+# print(defineDeck())
 players = allPlayers()
-print ("Players are:" + str(players))
+print("Players are:" + str(players))
 deck = defineDeck()
-#kortit1 = deal1(players, deck)
-#kortit2 = deal2(players, deck)
-#print(kortit1)
-#print(kortit2)
-number1(deal1)
+# kortit1 = deal1(players, deck)
+# kortit2 = deal2(players, deck)
+# print(kortit1)
+# print(kortit2)
+number1(deal1(players, deck, ))
 number2(deal2)
 print(str(deal1))
-#compare1(deal1)
-#compare2(deal2)
+# compare1(deal1)
+# compare2(deal2)
